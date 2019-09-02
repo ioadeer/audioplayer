82d9b6d6416c043924bcc8247b9bbb9ff62cef7e
Jim Reed <cjoackin@gmail.com>
2019-09-02 20:31:04 -0300

    Agregue fileDialog, para abrir archivos, ver el nombre, play y stop mejorados

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import pyaudio
 import wave
 import atexit
 from random import randrange
-from PyQt5.QtWidgets import QApplication, QDialog
+from PyQt5.QtWidgets import QApplication, QDialog, QFileDialog
 from PyQt5.QtCore import QTimer
 from guiTest1 import *
 
@@ -16,17 +16,14 @@ class AudioPlayer(object):
     def __init__(self):
         self.CHUNK = 8192 #1024 
         self.START_POINT = 0
-        #self.wf = wave.open('01_otto_muhl.wav', 'rb')
-        self.wf = wave.open('03_Hellucination_1_Drowingirl_mono.wav', 'rb')
+        self.frames = 0
         # aca estoy hardcodeando el stream a las caracteristicas del primer audio
         # probar format = pyaudio.paInt16, channels =2 , rate 44100
         self.p = pyaudio.PyAudio()
-        self.stream = self.p.open(format=self.p.get_format_from_width(self.wf.getsampwidth()),#pyaudio.paInt16,
+        self.stream = self.p.open(format=pyaudio.paInt16, 
                             channels = 1,
                             rate = 44100,
                             output = True)
-        self.frames = self.wf.readframes(self.wf.getnframes())
-        self.number_of_frames = self.wf.getnframes()
         atexit.register(self.close)
 
     def write_output(self):
@@ -34,6 +31,12 @@ class AudioPlayer(object):
         output = self.frames[self.START_POINT:self.START_POINT+self.CHUNK]
         self.START_POINT += self.CHUNK
         self.stream.write(output)
+
+    def load_file_and_set_frames(self, file_path):
+        # agregar excepcion si no lo puede cargar
+        self.wf = wave.open(file_path, 'rb')
+        self.frames = self.wf.readframes(self.wf.getnframes())
+        self.number_of_frames = self.wf.getnframes()
 
     def close(self):
         self.stream.stop_stream()
@@ -45,25 +48,43 @@ class MyForm(QDialog):
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
         self.ui.readPoint.valueChanged.connect(self.readPointValueChanged)
-        self.ui.playSound.toggled.connect(self.turnPlayStatusOn)
-        self.ui.playSound.setChecked(True)
-        self.status =True 
+        self.ui.playButton.toggled.connect(self.turnPlayStatusOn)
+        self.ui.playButton.setChecked(False)
+        self.ui.stopButton.setChecked(True)
+        self.status = False 
         self.value = 0
+        self.ui.pushButtonLoadFile.clicked.connect(self.openFileDialog)
         self.show()
         self.initAudioPlayer()
-        maximum = int(self.player.number_of_frames / self.player.CHUNK)
-        self.ui.readPoint.setMaximum(maximum)
         timer = QtCore.QTimer()
         timer.timeout.connect(self.audioCallback)
-        timer.start(0.2)
         self.timer = timer
+
+    def openFileDialog(self):
+        self.ui.stopButton.setChecked(True)
+        self.status = False
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fname = QFileDialog.getOpenFileName(self, 'Open File','\home','', options = options)
+        if fname[0]:
+            f = open(fname[0], 'r')
+        with f:
+            self.player.load_file_and_set_frames(f.name)
+            self.player.START_POINT = 0 
+            file_name_from_path = f.name.split('/')[-1]
+            self.ui.labelFileName.setText(file_name_from_path)
+            maximum = int(self.player.number_of_frames / self.player.CHUNK)
+            print(maximum)
+            self.ui.readPoint.setMaximum(maximum)
+            self.ui.readPoint.setValue(0)
+            self.timer.start(0.2)
 
     def readPointValueChanged(self, value):
         self.player.START_POINT = self.player.CHUNK * value
         #print(self.player.START_POINT)
 
     def turnPlayStatusOn(self, status):
-        self.status = self.ui.playSound.isChecked()
+        self.status = self.ui.playButton.isChecked()
         #print(status)
     
     def initAudioPlayer(self):
@@ -77,6 +98,8 @@ class MyForm(QDialog):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     w = MyForm()
-    w.exec()
-    sys.exit(0)
+    w.show()
+    #w.exec()
+    #sys.exit(0)
+    sys.exit(app.exec_())
 
